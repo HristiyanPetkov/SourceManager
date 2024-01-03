022da0caeda709c3896478f701b315bc6b73084a
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-01-03 09:47:54 +0200

    Basic frontend structure and connection with backend

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI, Request
-from starlette.templating import Jinja2Templates
 
 from database import Base, engine
 from models import user, organization, source
 from routers import sources, users, organizations
-
-templates = Jinja2Templates(directory="templates")
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configure CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # Replace with your React app's URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def startup_event():
@@ -24,6 +35,6 @@ app.include_router(organizations.router)
 app.include_router(sources.router)
 
 
-@app.get('/')
-def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
