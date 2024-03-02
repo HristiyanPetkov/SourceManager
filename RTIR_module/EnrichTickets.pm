20f9cd9e90e83290c0a80fa5ed4128696ceb414c
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-03-03 01:31:01 +0200

    Additional information on the tickets
    Added comment form the user table into the tickets.
    
    Removed test file.

--- RTIR_module/EnrichTickets.pm
+++ RTIR_module/EnrichTickets.pm
@@ -22,21 +22,31 @@ sub _EnrichTicket {
     my $dbh = DBI->connect("dbi:SQLite:dbname=$db_file", "", "") or die $DBI::errstr;
 
     my $sth = $dbh->prepare("
-        SELECT sources.type, sources.value, organizations.name AS organization_name
+        SELECT sources.type, sources.value, organizations.name AS organization_name, users.name AS user_name, users.phone AS user_phone,users.comment AS user_comment
         FROM sources
         JOIN organizations ON sources.organization_id = organizations.id
+        JOIN users ON sources.user_id = users.id
         WHERE sources.value = ?
     ");
     $sth->execute($host_name);
 
     if (my $row = $sth->fetchrow_hashref) {
         my $organization_name = $row->{organization_name};
+        my $user_name = $row->{user_name};
+        my $user_phone = $row->{user_phone};
+        my $user_comment = $row->{user_comment};
 
         RT::Logger->info("Organization name: " . $organization_name);
+        RT::Logger->info("User: " . $user_name . ", " . $user_phone . ", " . $user_comment);
 
         my $org_custom_field = RT::CustomField->new(RT->SystemUser);
         $org_custom_field->LoadByName(Name => 'Organization');
         $ticket->AddCustomFieldValue(Field => $org_custom_field, Value => $organization_name);
+
+        my $user_info = $user_name . "(" . $user_phone . "): " . $user_comment;
+        my $user_custom_field = RT::CustomField->new(RT->SystemUser);
+        $user_custom_field->LoadByName(Name => 'Comment');
+        $ticket->AddCustomFieldValue(Field => $user_custom_field, Value => $user_info);
     } else {
         my $org_custom_field = RT::CustomField->new(RT->SystemUser);
         $org_custom_field->LoadByName(Name => 'Organization');
