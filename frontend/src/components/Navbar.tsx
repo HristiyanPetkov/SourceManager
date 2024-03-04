2a7d00bf51404666d08ed0750a85dc343d83206b
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-03-04 22:34:16 +0200

    Moved Logout to UserActions and Profile Management

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,39 +1,38 @@
-import React from 'react';
 import { useFilter } from '../context/FilterContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-import {Logout} from "./Logout";
+import { UserActions } from "./UserActions";
 
 export const Navbar: React.FC = () => {
   const { filter, setFilter } = useFilter();
 
   const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFilter(event.target.value);
+      setFilter(event.target.value);
   };
 
-  // add logout button
   return (
     <div className="fixed top-0 left-0 w-full">
-      <header className="bg-dark-blue py-2">
-        <div className="container mx-auto flex items-center justify-between">
-          <a href="/home" className="flex items-center text-white hover:no-underline">
-            <FontAwesomeIcon icon={faHome} className="w-6 h-6 m-2" />
-            <h1 className="text-xl font-bold ml-2">Source Manager</h1>
-          </a>
+        <nav>
+          <header className="bg-dark-blue py-2">
+              <div className="container mx-auto flex items-center justify-between">
+                  <a href="/home" className="flex items-center text-white hover:no-underline">
+                      <FontAwesomeIcon icon={faHome} className="w-6 h-6 m-2"/>
+                      <h1 className="text-xl font-bold ml-2">Source Manager</h1>
+                  </a>
 
-          <input
-            type="text"
-            placeholder="Filter hosts"
-            className="border p-2 rounded-md"
-            value={filter}
-            onChange={handleFilterChange}
-          />
+                  <input
+                      id="filter"
+                      type="text"
+                      placeholder="Filter hosts"
+                      className="border p-2 rounded-md"
+                      value={filter}
+                      onChange={handleFilterChange}
+                  />
 
-          <div className="font-bold text-xl flex items-center text-white hover:no-underline mr-4">
-            <Logout />
-          </div>
-        </div>
-      </header>
+                  <UserActions />
+              </div>
+          </header>
+        </nav>
     </div>
   );
 };
