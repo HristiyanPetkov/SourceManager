f6490a7611b13c8656dd0b3c51abbcd222500b65
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-05-21 13:00:45 +0300

    [UPDATE] Callback URL for Keycloak is set dynamically

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -4,8 +4,9 @@ import {CLIENT_ID, KEYCLOAK_REALM, KEYCLOAK_URL} from "../config/KeyCloakConfig"
 export const Login = () => {
 
     useEffect(() => {
-        const authorization_url = `${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/auth?response_type=code&client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000/login/callback`;
-        window.location.href = (authorization_url);
+        const base_url = window.location.origin;
+        const callback_uri = `${base_url}/login/callback`;
+        window.location.href = `${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/auth?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(callback_uri)}`;
     }, []);
 
     return (
